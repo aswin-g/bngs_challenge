a41ee47b91a75dc54a3e12c7c03f193b28e2b02c
aswin-g <g.aswanikumar@gmail.com>
2021-07-22 22:44:05 +1000

    [BNGS-102] Document load test results

--- automated/load/load_search_ramping_up_down_vus.js
+++ automated/load/load_search_ramping_up_down_vus.js
@@ -10,6 +10,5 @@ export let options = {
 };
 export default function () {
   http.get('https://www.bunnings.com.au/search/products?q=hammer&sort=BoostOrder&page=1');
-  check(res, { 'status was 200': (r) => r.status == 200 });
   sleep(1);
 }
